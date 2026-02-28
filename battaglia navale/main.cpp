a315cb0471cf758b8a00e1513f98c1af69cb1194
Fouad <fouadre2317@gmail.com>
2026-02-28 15:47:53 +0100

    Risolto problema del sfarfallio e aggiunto opzioni di uscitaRisolto problema del sfarfallio e aggiunto opzioni di uscita

--- battaglia navale/main.cpp
+++ battaglia navale/main.cpp
@@ -19,12 +19,19 @@ const int MAIN_FG = 0;
 const int INPUT_FG = 15;
 const int INFO_FG = 5;
 const int ERROR_FG = 4;
-int c;
-int i = 9;
+
 bool maxaltezza = true;
 bool minaltezza = false;
+bool uscire = false;
+
+int c;
+int i = 9;
+int z = 0;
 
 void displayMainLayout();
+void INIZIO();
+void Gnavi();
+void USCITA();
 void hideCursor();
 
 HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -36,70 +43,152 @@ int main()
     keybd_event(VK_RETURN, 0x1c,KEYEVENTF_KEYUP,0);  //          | ------> schermo intero all'avvio
     keybd_event(VK_MENU, 0x38,KEYEVENTF_KEYUP,0);    // ---------|
     hideCursor();
-    displayMainLayout();
+    clearScreen(MAIN_BG, MAIN_FG);
+    while (!uscire)
+    {
+        displayMainLayout();
+    }
+    USCITA ();
+    Sleep(4000);
     return 0;
 }
 
-void hideCursor() {
+void displayMainLayout()
+{
+    setColor(MAIN_BG, MAIN_FG);
+    gotoXY(0, 0);
+    cout << "Battaglia navale                                           - Data: ";
+    displayToday();
+    gotoXY(0, 1);
+    cout << "================================================================================";
+    if (z == 0){
+        gotoXY(31, i);
+        cout << ">";
+    }else{
+        gotoXY(31,z);
+        cout <<" ";
+        gotoXY(31,i);
+        cout <<">";
+    }
+    if(i == 9)
+    {
+        SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        gotoXY(32, 9);
+        cout << "Inizia";
+        SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+    }
+    else
+    {
+        gotoXY(32, 9);
+        cout << "Inizia";
+    }
+    if(i == 10)
+    {
+        SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        gotoXY(32, 10);
+        cout << "Gestione navi";
+        SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+    }
+    else
+    {
+        gotoXY(32, 10);
+        cout << "Gestione navi";
+    }
+    if(i == 11)
+    {
+        SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+        gotoXY(32, 11);
+        cout << "Esci";
+        SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
+    }
+    else
+    {
+        gotoXY(32, 11);
+        cout << "Esci";
+    }
+    gotoXY(0,20);
+    cout << "Premere ESC per uscire dal programma";
+    gotoXY(0, 21);
+    cout << "================================================================================";
+    c = _getch();
+    if (i == 9)
+    {
+        maxaltezza = true;
+    }
+    else if (i == 11)
+    {
+        minaltezza = true;
+    }
+    if (c == SU && !maxaltezza)
+    {
+        z = i;
+        i -= 1;
+        minaltezza = false;
+    }
+    else if (c == GIU && !minaltezza)
+    {
+        z = i;
+        i +=1 ;
+        maxaltezza = false;
+    }
+    if (c == ENTER){
+        switch (i){
+            case 9:
+                INIZIO();
+                break;
+            case 10:
+                Gnavi();
+                break;
+            case 11:
+                uscire = true;
+                break;
+        }
+    }
+    if (c == ESC)
+    {
+        uscire = true;
+    }
+}
+
+void INIZIO ()
+{
+
+}
+
+void Gnavi()
+{
+
+}
+
+void USCITA ()
+{
+    clearScreen( 0, 15);
+    SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+    cout <<R"(
+ _____ ______  ___   ___________ _____     ______ ___________       ___  _   _ ___________
+|  __ \| ___ \/ _ \ |___  /_   _|  ___|    | ___ \  ___| ___ \     / _ \| | | |  ___| ___ \
+| |  \/| |_/ / /_\ \   / /  | | | |__      | |_/ / |__ | |_/ /    / /_\ \ | | | |__ | |_/ /
+| | __ |    /|  _  |  / /   | | |  __|     |  __/|  __||    /     |  _  | | | |  __||    /
+| |_\ \| |\ \| | | |./ /____| |_| |___     | |   | |___| |\ \     | | | \ \_/ / |___| |\ \
+ \____/\_| \_\_| |_/\_____/\___/\____/     \_|   \____/\_| \_|    \_| |_/\___/\____/\_| \_|
+
+
+ _____ _____ _____ _____   ___ _____ _____        ___     ______  ___ _____ _____ ___  _____  _     _____  ___        _   _   ___  _   _  ___   _      _____
+|  __ \_   _|  _  /  __ \ / _ \_   _|  _  |      / _ \    | ___ \/ _ \_   _|_   _/ _ \|  __ \| |   |_   _|/ _ \      | \ | | / _ \| | | |/ _ \ | |    |  ___|
+| |  \/ | | | | | | /  \// /_\ \| | | | | |     / /_\ \   | |_/ / /_\ \| |   | |/ /_\ \ |  \/| |     | | / /_\ \     |  \| |/ /_\ \ | | / /_\ \| |    | |__
+| | __  | | | | | | |    |  _  || | | | | |     |  _  |   | ___ \  _  || |   | ||  _  | | __ | |     | | |  _  |     | . ` ||  _  | | | |  _  || |    |  __|
+| |_\ \_| |_\ \_/ / \__/\| | | || | \ \_/ /     | | | |   | |_/ / | | || |   | || | | | |_\ \| |_____| |_| | | |     | |\  || | | \ \_/ / | | || |____| |___
+ \____/\___/ \___/ \____/\_| |_/\_/  \___/      \_| |_/   \____/\_| |_/\_/   \_/\_| |_/\____/\_____/\___/\_| |_/     \_| \_/\_| |_/\___/\_| |_/\_____/\____/
+
+
+)";
+}
+
+void hideCursor()
+{
     HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO cursorInfo;
     GetConsoleCursorInfo(console, &cursorInfo);
     cursorInfo.bVisible = FALSE;
     SetConsoleCursorInfo(console, &cursorInfo);
-}
-
-void displayMainLayout() {
-    while(true){
-        clearScreen(MAIN_BG, MAIN_FG);
-        setColor(MAIN_BG, MAIN_FG);
-        gotoXY (0, 0);
-        cout << "Battaglia navale                                           - Data: ";
-        displayToday();
-        gotoXY(0, 1);
-        cout << "================================================================================";
-        gotoXY(31, i);
-        cout << ">";
-        if(i==9){
-            SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
-            gotoXY(32, 9);
-            cout << "Inizia";
-            SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
-        }else{
-            gotoXY(32, 9);
-            cout << "Inizia";
-        }
-        if(i==10){
-            SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
-            gotoXY(32, 10);
-            cout << "Gestione navi";
-            SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
-        }else{
-            gotoXY(32, 10);
-            cout << "Gestione navi";
-        }
-        if(i==11){
-            SetConsoleTextAttribute(h, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
-            gotoXY(32, 11);
-            cout << "Esci";
-            SetConsoleTextAttribute(h, BACKGROUND_GREEN | BACKGROUND_BLUE | BACKGROUND_INTENSITY);
-        }else{
-            gotoXY(32, 11);
-            cout << "Esci";
-        }
-        gotoXY(0, 21);
-        cout << "================================================================================";
-        c = getch();
-        if (i==9){
-            maxaltezza = true;
-        }else if (i==11){
-            minaltezza = true;
-        }
-        if(c==SU && !maxaltezza){
-            i-=1;
-            minaltezza = false;
-        }else if(c==GIU && !minaltezza){
-            i+=1;
-            maxaltezza = false;
-        }
-    }
 }
